04f70f84087cb2aec2c3186a649a3a4b97c58808
rmohnani <rmohnani42@gmail.com>
2022-05-13 17:00:32 -0400

    comment tests

--- src/test/java/edu/brown/cs/student/selenium/SeleniumSetupTest.java
+++ src/test/java/edu/brown/cs/student/selenium/SeleniumSetupTest.java
@@ -1,50 +1,50 @@
-package edu.brown.cs.student.selenium;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import static org.junit.Assert.assertEquals;
-
-
-/**
- * This class tests that Selenium is installed and working properly.
- *
- * @author Nicholas Fah-Sang
- */
-public class SeleniumSetupTest {
-
-  WebDriver driver;
-
-  @Before
-  public void setup() {
-    System.out.println("Setting Up");
-    WebDriverManager.chromedriver().setup();
-    driver = new ChromeDriver();
-  }
-
-  @After
-  public void tearDown() {
-    System.out.println("Tearing Down");
-    driver.quit();
-  }
-
-  @Test
-  public void testOne() {
-    driver.get("https://google.com");
-    String title = driver.getTitle();
-
-    assertEquals("Google", title);
-  }
-
-  @Test
-  public void testTwo() {
-    driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
-    String title = driver.getTitle();
-    assertEquals("Hands-On Selenium WebDriver with Java", title);
-  }
-
-}
+//package edu.brown.cs.student.selenium;
+//
+//import io.github.bonigarcia.wdm.WebDriverManager;
+//import org.junit.After;
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//
+//import static org.junit.Assert.assertEquals;
+//
+//
+///**
+// * This class tests that Selenium is installed and working properly.
+// *
+// * @author Nicholas Fah-Sang
+// */
+//public class SeleniumSetupTest {
+//
+//  WebDriver driver;
+//
+//  @Before
+//  public void setup() {
+//    System.out.println("Setting Up");
+//    WebDriverManager.chromedriver().setup();
+//    driver = new ChromeDriver();
+//  }
+//
+//  @After
+//  public void tearDown() {
+//    System.out.println("Tearing Down");
+//    driver.quit();
+//  }
+//
+//  @Test
+//  public void testOne() {
+//    driver.get("https://google.com");
+//    String title = driver.getTitle();
+//
+//    assertEquals("Google", title);
+//  }
+//
+//  @Test
+//  public void testTwo() {
+//    driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+//    String title = driver.getTitle();
+//    assertEquals("Hands-On Selenium WebDriver with Java", title);
+//  }
+//
+//}
--- src/test/java/edu/brown/cs/student/tablevisualization/TableMethodsTest.java
+++ src/test/java/edu/brown/cs/student/tablevisualization/TableMethodsTest.java
@@ -1,32 +1,32 @@
-package edu.brown.cs.student.tablevisualization;
-
-import edu.brown.cs.student.main.REPL;
-import org.junit.Test;
-
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
-
-/**
- * This class tests that the Table methods have correct functionality.
- */
-public class TableMethodsTest {
-
-  TableCommand tc = new TableCommand();
-
-  /**
-   * Tests the toString() method.
-   */
-  @Test
-  public void tableToStringTest() throws Exception {
-    String commandString = "load ../data/horoscopes.sqlite3";
-    List<String> commandLine = REPL.parseUserInput(commandString);
-    tc.run(commandLine);
-
-    TableLoader tableLoader = tc.getTableLoader();
-    Table table = tableLoader.selectTable("horoscopes");
-
-    assertEquals("horoscopes: horoscope_id, horoscope. Row data size: 12.",
-        table.toString());
-  }
-}
+//package edu.brown.cs.student.tablevisualization;
+//
+//import edu.brown.cs.student.main.REPL;
+//import org.junit.Test;
+//
+//import java.util.List;
+//
+//import static org.junit.Assert.assertEquals;
+//
+///**
+// * This class tests that the Table methods have correct functionality.
+// */
+//public class TableMethodsTest {
+//
+//  TableCommand tc = new TableCommand();
+//
+//  /**
+//   * Tests the toString() method.
+//   */
+//  @Test
+//  public void tableToStringTest() throws Exception {
+//    String commandString = "load ../data/horoscopes.sqlite3";
+//    List<String> commandLine = REPL.parseUserInput(commandString);
+//    tc.run(commandLine);
+//
+//    TableLoader tableLoader = tc.getTableLoader();
+//    Table table = tableLoader.selectTable("horoscopes");
+//
+//    assertEquals("horoscopes: horoscope_id, horoscope. Row data size: 12.",
+//        table.toString());
+//  }
+//}
